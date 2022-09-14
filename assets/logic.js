47d15fc29b0c0aefad466bf302b0931893a6e1be
Lux <anguyen780@gmail.com>
2022-09-13 21:48:26 -0500

    worked on local storage

--- assets/logic.js
+++ assets/logic.js
@@ -6,13 +6,34 @@ var tempEl = $('#temperature');
 var windEl = $('#wind');
 var humEl = $('#humidity');
 var forecastBox = $('#forecast-div');
-var dateMoment = moment().add(1, 'day').format("MMM Do YY");
+var cityHistory = $('#city-history');
+
 
 
 function submission(event){
     event.preventDefault();
     var cityInput = cityInputEl.val();
-    cityInputEl.val('');
+    var savedCity = {
+        city: cityInputEl.val()
+    }
+    var cityArray = JSON.parse(localStorage.getItem('savedCity')) || [];
+    cityArray.push(savedCity);
+    localStorage.setItem('savedCity', JSON.stringify(cityArray));
+
+    for (let i = 0; i < cityArray.length; i++) {
+        var dateButton = $('<button>').attr('class','btn btn-dark');
+        var objectNum = Object.keys(cityArray);
+        dateButton.text(`${cityArray[i]}`);
+        cityHistory.append(dateButton);
+        
+        
+
+        
+    }
+    
+    
+
+
     var weatherApi = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&units=imperial&appid=${apiKey}`;
     forecastBox.addClass('border border-dark')
     
@@ -29,18 +50,52 @@ function submission(event){
         windEl.text(`Wind Speed: ${wind} MPH`);
         humEl.text(`Humidity: ${humidity} %`);
     })
+    forecast5Day();
 }
 
-function forecast5Day (event){
-    event.preventDefault();
+function forecast5Day (){
+    var cityInput = cityInputEl.val();
+    cityInputEl.val('');
     var weatherApi = `https://api.openweathermap.org/data/2.5/forecast?q=${cityInput}&units=imperial&appid=${apiKey}`;
-    $('#5day-h2').text('5 Day Forecast')
+    $('#5day-h2').text('5 Day Forecast');
 
     fetch(weatherApi).then(function(response){
         return response.json();
     }).then(function(results){
+        forecastArray = [];
+        forecastArray.push(results.list[6]);
+        forecastArray.push(results.list[14]);
+        forecastArray.push(results.list[22]);
+        forecastArray.push(results.list[30]);
+        forecastArray.push(results.list[38]);
         
+        for (let i = 0; i < forecastArray.length; i++) {
+            var forecastDayIcon = forecastArray[i].weather[0].icon;
+            var forecastDayImg = `http://openweathermap.org/img/wn/${forecastDayIcon}.png`;
+            var dayArray = forecastArray[i].dt_txt.split(' ')[0].split('-');
+            var dayTxt = $('<h5>').text(`${dayArray[1]}/${dayArray[2]}/${dayArray[0]}`);
+            var temp5Day = $('<p>').text(`Temp: ${forecastArray[i].main.temp} °F`);
+            var wind5Day = $('<p>').text(`Wind Speed: ${forecastArray[i].wind.speed} MPH`);
+            var humidity5Day = $('<p>').text(`Humidity: ${forecastArray[i].main.humidity} %`);
+            $('#card-display').attr('class', 'card custom-card col-8');
+            var iconImg = $('<img>').attr('src', forecastDayImg);
+            var weatherDisplay = $('#card-display');
+            var weatherCard = $('<div>').attr('class','card-body bg-dark text-white');
+            var weatherContainer = $('<div>').attr('class','card');
+            weatherContainer.attr('style','width: 18rem');
+            weatherCard.append(dayTxt, iconImg, temp5Day, wind5Day, humidity5Day);
+            weatherContainer.append(weatherCard);
+            weatherDisplay.append(weatherContainer);
+           
+            
+        }
     })
+    // saveCity();
+}
+
+function saveCity (){
+    
+    
 }
 
 searchButtonEl.on('click', submission);
